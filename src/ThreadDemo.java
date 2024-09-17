9e6e451b3e2eb5e5fde99451dba341d95e172e1d
Prince Patel <ppnitjsr@gmail.com>
2024-09-17 12:06:14 +0530

    Video 2, Thread Using Thread.class

--- src/ThreadDemo.java
+++ src/ThreadDemo.java
@@ -1,5 +1,40 @@
+class Hi extends Thread{
+    public void run(){
+        for(int i=1;i<=5;i++){
+            System.out.println("Hi");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+class Hello extends Thread{
+    @Override
+    public void run(){
+        for(int i=1;i<=5;i++){
+            System.out.println("Hello");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
 public class ThreadDemo {
     public static void main(String[] args) {
+        Hi obj1 = new Hi();
+        Hello obj2 = new Hello();
+
+        //this start()  Start Thread and internally call run()(start always call run)
+        obj1.start();
+        obj2.start();
+
+
 
     }
 }
